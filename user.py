610ac4fa8d200fa5914645d6e4bb7e416a8a48e5
chastine1 <batamurizashastine@gmail.com>
2019-02-11 14:33:59 +0200

    I make 3 test

--- user.py
+++ user.py
@@ -12,3 +12,20 @@ class User:
         self.last_name = last_name
         self.username= username
         self.password = password
+
+        user_list = [] # Empty user list
+ # Init method up here
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
+    def delete_user(self):
+
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+
+        User.user_list.remove(self)
--- user_test.py
+++ user_test.py
@@ -26,6 +26,43 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.last_name,"bayizere")
         self.assertEqual(self.new_user.username,"tinabayi")
         self.assertEqual(self.new_user.password,"wecode2019kie")
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(User.user_list),1)
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []
+
+
+
+    def test_save_multiple_user(self):
+            '''
+            test_save_multiple_user to check if we can save multiple user
+            objects to our user_list
+            '''
+            self.new_user.save_user()
+            test_user = User("christine","bayizere","bayizerechristine","Kansangire81") # new user
+            test_user.save_user()
+            self.assertEqual(len(User.user_list),2)
+
+    def test_delete_user(self):
+            '''
+            test_delete_user to test if we can remove a user from our user list
+            '''
+            self.new_user.save_user()
+            test_user = User("Christine","bayizere","bayizerechristine","Kansangire81") # new user
+            test_user.save_user()
+
+            self.new_user.delete_user()# Deleting a user object
+            self.assertEqual(len(User.user_list),1)
+
+            
 
 
 if __name__ == '__main__':
